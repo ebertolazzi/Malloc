93fc083ddf1e0315b27b35707c1ba563e80ed418
Enrico Bertolazzi <enrico.bertolazzi@unitn.it>
2023-11-30 21:08:11 +0100

    added Eigen::Transpose formatting with {fmt}

--- src/Utils_eigen.hh
+++ src/Utils_eigen.hh
@@ -52,6 +52,12 @@ namespace fmt {
 
   template <typename PlainObjectType, int MapOptions, typename StrideType>
   struct formatter<Eigen::Map<PlainObjectType,MapOptions,StrideType>> : ostream_formatter {};
+
+  template <typename MAT>
+  struct formatter<Eigen::Transpose<MAT>> : ostream_formatter {};
+
+  template <typename EXPR>
+  struct formatter<Eigen::WithFormat<EXPR>> : ostream_formatter {};
 }
 
 #ifdef __clang__
